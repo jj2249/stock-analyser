93c0f11a763d4d955102e3a3c0a4feefdbe028d4
jj249 <joseph123johnson@outlook.com>
2020-12-11 11:22:48 +0000

    python data collection and plotting ready for time series analysis

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,25 +3,13 @@ import os
 import pandas as pd
 import quandl
 from quandl.errors.quandl_error import NotFoundError 
-import re
 import sys
-
-"""
-def save_as_csv(infile, outfile):
-    # open an xlsx file and save a copy as a csv. Handles all file closing internally
-
-    # check file extensions
-    if (re.search('_(\d+)\.xlsx$', infile) and re.search('_(\d+)\.csv$', outfile)):
-        df = pd.read_excel(infile)
-        df.to_csv(outfile) #default separator is a comma
-
-    else:
-        print("invalid file names")
-        sys.exit() # okay to exit as no files have been opened
-"""
 
 
 def read_csv(file_path, specifyCols=None):
+    """
+    reads a csv file into a Pandas dataframe. default argument allows for specific columns to be extraced
+    """
     data  = pd.read_csv(file_path, usecols=specifyCols)
     return data
 
@@ -33,78 +21,75 @@ def prepend_database_stock(stock_key, database):
     return database + '/' + stock_key
 
 
-def prepend_database_multiple_stocks(stock_keys, database):
-    """
-    takes in a list of stock tickers and prepends the database to each. database must be in string format
-    """
-    for index, ticker in enumerate(stock_keys):
-        stock_keys[index] = database + '/' + ticker
-    return stock_keys
-
-
 def retrieve_stock_data(stock_key, start_d=None, end_d=None, sampling_rate=None, use_cache=False):
     """
     uses the quandl API to fetch online stock data, option to use cache to avoid repeated requests to the quandl
     server. Note: Can only use cache when considering one stock as it will overwrite file each time
     """
     print('Loading data...')
-    # authenticate requests using API key
-    quandl.ApiConfig.api_key = 'MLEChCzx-xjxjkefxwpi'
 
-    dir = 'cache'
+    # authenticate requests using API key
+    quandl.ApiConfig.api_key = "MLEChCzx-xjxjkefxwpi"
+
     # check that cache exists by trying to make it
+    dir = 'cache'
     try:
         os.makedirs(dir)
     except FileExistsError:
         pass
 
-    # add file to existing directory
+    # add cache file to cache directory
     cachef = os.path.join(dir, 'stock_data.npy')
 
     # check for cache file and if it is not found then fetch online data and dump to cache
     if use_cache:
         try:
             data = np.load(cachef, 'r')
+            print('Cache used successfully')
         except FileNotFoundError:
             data = quandl.get(stock_key, returns='numpy', start_date=start_d, end_date=end_d, collapse=sampling_rate)
             np.save(cachef, data)
+            print('Cache used unsuccessfully')
+
 
     # fetch online data and ignore cache
     else:
         data = quandl.get(stock_key, returns='numpy', start_date=start_d, end_date=end_d, collapse=sampling_rate)
-        #np.save(cachef, data)
 
-    # data is in tuple pairs stored in numpy array so separate
-    #t = np.array([item[0] for item in data])
-    s = np.array([item[4] for item in data])
-
-    return s
+    return data
 
 
-def api_call(csv_data, final_csv):
-	full_data = pd.DataFrame()
-	stock_names = read_csv(csv_data, specifyCols=['Ticker', 'Start Date', 'End Date'])
-	stock_tuples = list(stock_names.itertuples(index=False, name=None))
-	valid_count = 0
+def api_call_csv(csv_data, final_csv):
+    """
+    main function that takes in a pre-formatted csv and handles api calling, then writes collected data out to a new csv
+    """
+    # define global dataframe for final write-out
+    full_data = pd.DataFrame()
 
-	for stock in stock_tuples:
-		try:
-			temp = retrieve_stock_data(prepend_database_stock(str(stock[0]), 'WIKI'), stock[1], stock[2], sampling_rate='monthly')
-			temp_df = pd.DataFrame({str(stock[0]) : temp})
+    # parse input csv and zip data into 3-tuples
+    stock_names = read_csv(csv_data, specifyCols=['Ticker', 'Start Date', 'End Date'])
+    stock_tuples = list(stock_names.itertuples(index=False, name=None))
 
-			full_data = pd.concat([full_data, temp_df], axis=1, sort=False)
-			valid_count += 1
+    valid_count = 0
 
-		except NotFoundError:
-			pass
+    # retrieve price data for each stock and append it to the global data frame
+    for stock in stock_tuples:
+        try:
+            temp_data = retrieve_stock_data(prepend_database_stock(str(stock[0]), 'LBMA'), stock[1], stock[2], sampling_rate='monthly')
+            temp = np.array([item[4] for item in temp_data])
+            temp_df = pd.DataFrame({str(stock[0]) : temp})
 
-	full_data.to_csv(final_csv)
+            full_data = pd.concat([full_data, temp_df], axis=1, sort=False)
+            # if the call was valid, increment
+            valid_count += 1
 
+        except NotFoundError:
+            # pass if no entry is found in the database
+            pass
 
+    # write the global dataframe to a file and return the valid count
+    full_data.to_csv(final_csv)
+    return valid_count
 
-infile = "./stock_prices.csv"
-outfile = "./full_collected_data2.csv"
-
-api_call(infile, outfile)
-
-
+def api_call_python(stock_key, start_date=None, end_date=None, sampling_rate=None, use_cache=False):
+	return retrieve_stock_data(prepend_database_stock(stock_key, 'LBMA'), start_d=start_date, end_d=end_date, sampling_rate=sampling_rate, use_cache=use_cache)
---
+++ plotting.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from data_fetcher import *
+
+data_stock = api_call_python('GOLD', start_date=None, end_date=None, use_cache=True, sampling_rate='weekly')
+times_prices = np.array([(row[0], row[4]) for row in data_stock])
+times_prices_hsplit = np.array(np.hsplit(times_prices, 2))
+dates = np.array([times_prices_hsplit[0][i][0] for i in range(times_prices.shape[0])])
+closing_prices = np.array([times_prices_hsplit[1][i][0] for i in range(times_prices.shape[0])])
+
+
+plt.plot_date(dates, closing_prices, fmt='-b')
+plt.show()
